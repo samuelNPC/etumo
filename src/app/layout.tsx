d61ac3b957a2b70cda06d9dbd0101839fd6d0d67
samuelNPC <samwelampeire@gmail.com>
2026-06-18 10:00:33 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,12 +48,13 @@ export const metadata: Metadata = {
     images: ["/og-image.png"],
   },
   icons: {
-    icon: [
-      { url: "/favicon-48x48.png", sizes: "48x48", type: "image/png" },
-      { url: "/favicon.png", sizes: "192x192", type: "image/png" },
-    ],
-    apple: "/apple-touch-icon.png",
-  },
+  icon: [
+    { url: "/favicon-48x48.png", sizes: "48x48", type: "image/png" },
+    { url: "/favicon.png", sizes: "192x192", type: "image/png" },
+    { url: "/icon-512x512.png", sizes: "512x512", type: "image/png" },
+  ],
+  apple: "/apple-touch-icon.png",
+},
   manifest: "/manifest.webmanifest", // Links the PWA settings
 };
 
